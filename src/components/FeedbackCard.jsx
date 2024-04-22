a72098990c3558e0a90f131dfc2a9d616bfbcc57
karimbaggari <karimbaggari8@gmail.com>
2024-04-22 15:16:44 +0100

    update the landing page

--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -1,14 +1,20 @@
 import { quotes } from "../assets";
 import parse from 'html-react-parser'
 
-const FeedbackCard = ({ content, name, title, img }) => (
-  <div className="flex justify-between flex-col px-10 py-12 rounded-[20px]  max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card bg-indigo-900" >
-    <div className="w-full h-[30px] flex justify-center">
-    { parse(img) }
+const FeedbackCard = ({ content, name, img }) => (
+  <div className="flex justify-between px-10 py-12 rounded-[20px] flex-col max-w-[370px] max-h-[400px] relative md:mr-10 sm:mr-5 mr-0 my-10 feedback-card bg-indigo-900" >
+    <div className="w-[78%] h-[30px] flex justify-center absolute">
+      {parse(img)}
     </div>
-    <p className="font-poppins font-normal text-[18px] leading-[32.4px] text-white my-10">
+    <div className="w-full flex justify-center mt-4 h-full items-start">
+      {name}
+    </div>
+    <p className="font-poppins font-normal text-[18px] leading-[22.4px] text-white my-10 flex  flex-col justify-end h-full">
       {content}
     </p>
+    <div class="flex flex-col sm:flex-row sm:justify-center lg:justify-center">
+      <a href="#" class="inline-block rounded-lg bg-gray-200 px-8 py-3 text-center text-sm font-semibold text-indigo-900 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-300 focus-visible:ring active:text-gray-700 md:text-base">Learn More</a>
+    </div>
   </div>
 );
 
--- src/constants/index.js
+++ src/constants/index.js
@@ -23,21 +23,18 @@ export const features = [
   {
     id: "feature-1",
     icon: star,
-    title: "Quality",
     content:
       "The best credit cards offer some tantalizing combinations of promotions and prizes",
   },
   {
     id: "feature-2",
     icon: shield,
-    title: "100% Secured",
     content:
       "We take proactive steps make sure your information and transactions are secure.",
   },
   {
     id: "feature-3",
     icon: send,
-    title: "Fast Delievry",
     content:
       "A balance transfer credit card can save you a lot of money in interest charges.",
   },
@@ -49,8 +46,7 @@ export const services = [
     content:
       "Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver.",
     name: "Growth",
-    title: "Founder & Leader",
-    img: `  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    img: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6" />
   </svg>`,
   },
@@ -59,9 +55,8 @@ export const services = [
     content:
       "Money makes your life easier. If you're lucky to have it, you're lucky.",
     name: "Security",
-    title: "Founder & Leader",
-    img: `  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6" />
+    img: `<svg xmlns="http://www.w3.org/2000/svg" class="h-full w-full" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
   </svg>`,
   },
   {
@@ -69,9 +64,8 @@ export const services = [
     content:
       "It is usually people in the money business, finance, and international trade that are really rich.",
     name: "Cloud",
-    title: "Founder & Leader",
-    img: `  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6" />
+    img: `<svg xmlns="http://www.w3.org/2000/svg" class="h-full w-full" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 15a4 4 0 004 4h9a5 5 0 10-.1-9.999 5.002 5.002 0 10-9.78 2.096A4.001 4.001 0 003 15z" />
   </svg>`,
   },
   {
@@ -79,9 +73,8 @@ export const services = [
     content:
       "It is usually people in the money business, finance, and international trade that are really rich.",
     name: "Speed",
-    title: "Founder & Leader",
-    img: `  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6" />
+    img: `<svg xmlns="http://www.w3.org/2000/svg" class="h-full w-full" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z" />
   </svg>`,
   },
   {
@@ -89,9 +82,8 @@ export const services = [
     content:
       "It is usually people in the money business, finance, and international trade that are really rich.",
     name: "Support",
-    title: "Founder & Leader",
-    img: `  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6" />
+    img: `<svg xmlns="http://www.w3.org/2000/svg" class="h-full w-full" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18.364 5.636l-3.536 3.536m0 5.656l3.536 3.536M9.172 9.172L5.636 5.636m3.536 9.192l-3.536 3.536M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-5 0a4 4 0 11-8 0 4 4 0 018 0z" />
   </svg>`,
   },
   {
@@ -99,9 +91,8 @@ export const services = [
     content:
       "It is usually people in the money business, finance, and international trade that are really rich.",
     name: "24/7 Maintenance",
-    title: "Founder & Leader",
-    img: `  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6" />
+    img: `<svg xmlns="http://www.w3.org/2000/svg" class="h-full w-full" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
   </svg>`,
   },
 ];
